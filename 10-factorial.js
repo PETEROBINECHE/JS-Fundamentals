aaa0ad5b675d946cc8a330a1e5d50c8a03993102
OBINECHE PETER <Hoverexs@gmail.com>
2025-05-27 00:54:03 +0100

    Add recursive factorial function for Task 10

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
+
 /**
  * Recursive factorial function
- * @param {number} n - integer whose factorial we want
- * @returns {number} factorial of n
  */
 function factorial(n) {
   if (isNaN(n) || n === 0) {
-    // Factorial of NaN or 0 is 1
     return 1;
   }
   return n * factorial(n - 1);
@@ -14,3 +12,4 @@ function factorial(n) {
 
 const arg = parseInt(process.argv[2]);
 console.log(factorial(arg));
+
